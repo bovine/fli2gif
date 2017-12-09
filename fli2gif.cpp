730f8210044fb06207a7e0f7b2664de73eba6f22
Jeff Lawson <jeff@bovine.net>
2017-12-09 05:21:55 +0000

    replace old licensing information with BSD-style license.

--- fli2gif.cpp
+++ fli2gif.cpp
@@ -1,42 +1,8 @@
-// []-----------------------------------------------------------------------[]
-// |                                                                         |
-// |                                                                         |
-// |  This source and its associated executable are copyright by Jeff Lawson |
-// |  of JL Enterprises, however some portions of this code may or may not   |
-// |  have been originally inspired by other authors and credit is given if  |
-// |  due.  You are encouraged to redistribute this package, but you must    |
-// |  include all files originally distributed in this archive in their      |
-// |  original state and format and without modifications of any kind.       |
-// |  Under no circumstances may you make modifications to this product and  |
-// |  redistribute the resulting code or executables.  If you believe you    |
-// |  have made some useful or otherwise important enhancements and you      |
-// |  would like to share these changes with others, please contact the      |
-// |  author through one of the methods listed below.  Additionally, no      |
-// |  fees may be charged for the usage of this product by anyone other than |
-// |  the author of this file, except for fees (not to exceed US $10) by     |
-// |  disk distributors to cover disk duplication and handling.  If there    |
-// |  is any need to question this policy, please contact the author.        |
-// |  This source code and its executable are distributed without any kind   |
-// |  of warranty and the author may not be held accountable for damages of  |
-// |  any kind.                                                              |
-// |                                                                         |
-// |                                                                         |
-// |  I can reached via postal mail at:                                      |
-// |          Jeff Lawson                                                    |
-// |          1893 Kaweah Drive                                              |
-// |          Pasadena, CA 91105-2174                                        |
-// |          USA                                                            |
-// |                                                                         |
-// |  on the World Wide Web at:                                              |
-// |          http://members.aol.com/JeffLawson/                             |
-// |                                                                         |
-// |  or through E-mail at:                                                  |
-// |          jlawson@hmc.edu    or   JeffLawson@aol.com                     |
-// |                                                                         |
-// |  also via phone at:                                                     |
-// |          (213) 258-5604     or   (213) 258-4264                         |
-// |                                                                         |
-// []-----------------------------------------------------------------------[]
+// Fli2gif
+// Copyright (c) 1996, Jeff Lawson
+// All rights reserved.
+// See LICENSE for distribution information.
+
 
 // ******************
 // ***  Includes  ***
